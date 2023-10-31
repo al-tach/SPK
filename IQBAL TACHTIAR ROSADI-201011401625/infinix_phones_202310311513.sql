58449d9c66e282349cfc0fd7315df7a795acc4ba
al-tach <133991077+al-tach@users.noreply.github.com>
2023-10-31 15:33:51 +0700

    Add files via upload

---
+++ IQBAL TACHTIAR ROSADI-201011401625/infinix_phones_202310311513.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.infinix_phones (model,ram,processor,"storage",battery,price,screen_size) VALUES
+	 ('Infinix Zero 30 5G','12 GB','MediaTek Dimensity 8020','256 GB','5.000 mAh','Rp.4.299.000','6.78 inch'),
+	 ('Infinix Zero 30','8 GB','MediaTek Helio G99','256 GB','5.000 mAh','Rp.3.099.000','6.78 inch'),
+	 ('Infinix GT 10 pro 5G','8 GB','MediaTek Dimensity 8050','256 GB','5.000 mAh','Rp.3.899.000','6.67 inch'),
+	 ('Infinix Hot 30 Play','4 GB','MediaTek Helio G37','64 GB','6.000 mAh','Rp.1.249.000','6.82 inch'),
+	 ('Infinix Note 30 Pro','8 GB','MediaTek Helio G99','256 GB','5.000 mAh','Rp.2.948.000','6.67 inch'),
+	 ('Infinix Hot 30','8 GB','MediaTek Helio G88','128 GB','5.000 mAh','Rp.1.609.000','6.78 inch'),
+	 ('Infinix Not 30','8 GB','MediaTek Helio G99','256 GB','5.000 mAh','Rp.2.499.000','6.78 inch'),
+	 ('Infinix Hot 30 i','8 GB','MediaTek Helio G37','128 GB','5.000 mAh','Rp.1.499.000','6.6 inch'),
+	 ('Infinix Smart 7','3 GB','MediaTek Helio A22','64 GB','5.000 mAh','Rp.1.149.000','6.6 inch'),
+	 ('Infinix Hot 20 i','4 GB','MediaTek Helio G37','64 GB','5.000 mAh','Rp.1.599.000','6.6 inch');
